6fb1c1a901f23499096b55b77bb0491376178790
Shion Masuda <xxxxx@gmail.com>
2025-12-08 10:02:07 +0900

    feature: add dummy method /threads

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 from app.routers import threads
 
-app = FastAPI(
-    title="BBS API",
-    description="Simple Bulletin Board System API",
-    version="0.1"
-)
+app = FastAPI(title="BBS API - Step1")
 
 # ルーターを登録（まだ中身は空でOK）
 app.include_router(threads.router)
