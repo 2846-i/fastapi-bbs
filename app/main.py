6fd30161e7fbde1981c43c983f4d08f31c2f5981
Shion Masuda <xxxxx@gmail.com>
2025-12-17 10:17:12 +0900

    feature: スキーマの変更及び、スレッド作成APIのレスポンス追加

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+
 from app.routers import threads
+from app.routers import posts
 
-app = FastAPI(title="BBS API - Step1")
+app = FastAPI()
 
-# ルーターを登録（まだ中身は空でOK）
 app.include_router(threads.router)
+app.include_router(posts.router)
+app.include_router(posts.threads_router)
--- app/routers/threads.py
+++ app/routers/threads.py
@@ -10,22 +10,31 @@ router = APIRouter(
 @router.get("/", response_model=list[ThreadResponse])
 async def list_threads():
     return [
-        {"id": 1, "title": "ダミースレッド1", "created_at": "2025-11-20T00:00:00"},
-        {"id": 2, "title": "ダミースレッド2", "created_at": "2025-11-21T00:00:00"},
-    ]
+    {
+        "id": 1,
+        "title": "ダミースレッド1",
+        "created_at": datetime(2025, 11, 20, 0, 0, 0)
+    },
+    {
+        "id": 2,
+        "title": "ダミースレッド2",
+        "created_at": datetime(2025, 11, 21, 0, 0, 0)
+    },
+]
 
 @router.get("/{thread_id}", response_model=ThreadResponse)
 async def get_thread(thread_id: int):
     return {
-        "id": thread_id,
-        "title": f"ダミースレッド{thread_id}",
-        "created_at": "2025-11-21T00:00:00"
-    }
+    "id": thread_id,
+    "title": f"ダミースレッド{thread_id}",
+    "created_at": datetime(2025, 11, 21, 0, 0, 0)
+}
 
 @router.post("/", response_model=ThreadResponse)
 async def create_thread(thread: ThreadCreate):
     return {
-        "id": 999,
-        "title": thread.title,
-        "created_at": "2025-11-21T00:00:00",
-    }
+    "id": 999,
+    "title": thread.title,
+    "created_at": datetime(2025, 11, 21, 0, 0, 0)
+}
+
